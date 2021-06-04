bcf7a96627026864597c2add362080b119c21368
Yony Calsin <helloyonycalsin@gmail.com>
2021-06-04 14:33:11 -0500

    style: 💄 formating

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+    console.log('boop')
   }
-  return a + b;
-};
+  return a + b
+}
 
-console.log('Hello');
+console.log('Hello')
